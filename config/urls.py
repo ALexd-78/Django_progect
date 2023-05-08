2f62a93e51551a9a3c4765e096e2d20e54bf20fe
ALexd-78 <alexd-78@yandex.ru>
2023-05-08 18:36:01 +0300

    modify config\urls.py

--- config/urls.py
+++ config/urls.py
@@ -17,8 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from catalog.views import get_home, get_contacts
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('catalog.urls', namespace='catalog'))
+    path('', include('catalog.urls', namespace='catalog')),
+    path('home/', get_home, name='home'),
+    path('contacts/', get_contacts, name='get_contacts')
 ]
