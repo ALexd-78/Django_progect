0aa37ed734173402146ceb8ad0e4536bf6a1ac48
ALexd-78 <alexd-78@yandex.ru>
2023-05-22 02:39:55 +0300

    create and modify includes

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-# from catalog.views import index
+
 
 from catalog.views import home, contacts
 
@@ -9,7 +9,6 @@ from catalog.views import home, contacts
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    # path('', index),
     path('', home),
     path('contacts/', contacts),
 
