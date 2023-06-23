5fb436f284c9869a6d79430ce77ea619eab4cf2f
ALexd-78 <alexd-78@yandex.ru>
2023-06-23 03:48:53 +0300

    add app User

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
 
     'catalog',
     'catalog.services',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -132,4 +133,18 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# print(params)
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = 'nadal33der@yandex.ru'
+EMAIL_HOST_PASSWORD = 'ywsblqngxmqglbzu'
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+SERVER_EMAIL = EMAIL_HOST_USER
+EMAIL_ADMIN = EMAIL_HOST_USER
+
+AUTH_USER_MODEL = 'users.User'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
